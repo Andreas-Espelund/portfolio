56818c521167d6fb17f0a92faba5ed651880e453
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2023-10-30 11:45:40 +0100

    Dynamic birth date

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,10 +1,24 @@
 
 export default function Bio() {
+    function getAge() {
+        const birthDate = new Date("2000-01-17");
+        const today = new Date();
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        return age;
+    }
+
     return (
         <div className="flex flex-col justify-center gap-4">
             <h2 className="text-4xl font-semibold">Hey 👋</h2>
             <p className="text-xl">
-                My name is Andreas, i'm 23 years old and currently doing my masters in informatics at UiO.
+                My name is Andreas, i'm {getAge()} years old and currently doing my masters in informatics at UiO.
                 I would describe myself as a (aspiring) full stack developer, with a passion for back-end and low level stuff like networks and distributed systems.
             </p>
             <p className="text-xl">
