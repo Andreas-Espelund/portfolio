4b3a8e13ecf4fdf9ddaf4daee7b097634d726141
Andreas Raftevold Espelund <andreasesraf@gmail.com>
2025-06-15 20:51:20 +0200

    Remove unused function

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,21 +1,4 @@
 export default function Bio() {
-  function getAge() {
-    const birthDate = new Date("2000-01-17");
-    const today = new Date();
-
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDifference = today.getMonth() - birthDate.getMonth();
-
-    if (
-      monthDifference < 0 ||
-      (monthDifference === 0 && today.getDate() < birthDate.getDate())
-    ) {
-      age--;
-    }
-
-    return age;
-  }
-
   return (
     <div className="flex flex-col justify-center gap-4">
       <h2 className="text-4xl font-semibold">Hello 👋</h2>
